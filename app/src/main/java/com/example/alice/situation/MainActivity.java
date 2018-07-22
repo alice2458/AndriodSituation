78b4fef3fc83bdfc476362f40a9b8e8813147467
Alice2458 <18845788065@163.com>
2018-07-22 16:48:04 +0800

    lpp

--- app/src/main/java/com/example/alice/situation/MainActivity.java
+++ app/src/main/java/com/example/alice/situation/MainActivity.java
@@ -9,25 +9,88 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    private EditText edit;
-    private Button btn;
     private TextView showView;
+    private TextView note;
+    private EditText editNum1;
+    private EditText editNum2;
+    private Button btnAdd;
+    private Button btnSub;
+    private Button btnMul;
+    private Button btnDiv;
+    private int num1 = 0;
+    private int num2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edit = (EditText) findViewById(R.id.myed);
-        btn = (Button) findViewById(R.id.mybtn);
-        showView = (TextView) findViewById(R.id.mytext);
-        btn.setOnClickListener(new ShowListener());
+
+        showView = (TextView) findViewById(R.id.tvResult);
+        note = (TextView) findViewById(R.id.op);
+        editNum1 = (EditText) findViewById(R.id.etNum1);
+        editNum2 = (EditText) findViewById(R.id.etNum2);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSub = (Button) findViewById(R.id.btnSub);
+        btnMul = (Button) findViewById(R.id.btnMul);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+
+        btnAdd.setOnClickListener(new AddListener());
+        btnSub.setOnClickListener(new AddListener());
+        btnMul.setOnClickListener(new AddListener());
+        btnDiv.setOnClickListener(new AddListener());
+
+        editNum1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                MainActivity.this.editNum1.setText("");
+            }
+        });
+
+        editNum2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                MainActivity.this.editNum2.setText("");
+            }
+        });
     }
 
-    private class ShowListener implements View.OnClickListener{
+    private class AddListener implements View.OnClickListener{
         @Override
         public void onClick(View v){
-            String info = MainActivity.this.edit.getText().toString();
-            MainActivity.this.showView.setText("输入的内容是："+info);
+            MainActivity.this.num1 = Integer.parseInt(MainActivity.this.editNum1.getText().toString());
+            MainActivity.this.num2 = Integer.parseInt(MainActivity.this.editNum2.getText().toString());
+            MainActivity.this.note.setText("+");
+            MainActivity.this.showView.setText(String.valueOf(num1+num2));
+        }
+    }
+
+    private class DivListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            MainActivity.this.num1 = Integer.parseInt(MainActivity.this.editNum1.getText().toString());
+            MainActivity.this.num2 = Integer.parseInt(MainActivity.this.editNum2.getText().toString());
+            MainActivity.this.note.setText("除号");
+            MainActivity.this.showView.setText(String.valueOf(num1/num2));
+        }
+    }
+
+    private class SubListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            MainActivity.this.num1 = Integer.parseInt(MainActivity.this.editNum1.getText().toString());
+            MainActivity.this.num2 = Integer.parseInt(MainActivity.this.editNum2.getText().toString());
+            MainActivity.this.note.setText("-");
+            MainActivity.this.showView.setText(String.valueOf(num1-num2));
+        }
+    }
+
+    private class MulListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            MainActivity.this.num1 = Integer.parseInt(MainActivity.this.editNum1.getText().toString());
+            MainActivity.this.num2 = Integer.parseInt(MainActivity.this.editNum2.getText().toString());
+            MainActivity.this.note.setText("×");
+            MainActivity.this.showView.setText(String.valueOf(num1*num2));
         }
     }
 }
