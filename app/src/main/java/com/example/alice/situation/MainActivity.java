bdce00b45f7f8651ededd7cc17ec4ef24a437bfb
Alice2458 <18845788065@163.com>
2018-07-22 18:54:27 +0800

    lpp

--- app/src/main/java/com/example/alice/situation/MainActivity.java
+++ app/src/main/java/com/example/alice/situation/MainActivity.java
@@ -1,51 +1,37 @@
 package com.example.alice.situation;
 
 import android.app.Activity;
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-    private Button change;
-    private ImageView img;
+    private EditText password;
+    private CheckBox show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        change = (Button) findViewById(R.id.btnChang);
-        img = (ImageView) findViewById(R.id.img);
+        password = (EditText) findViewById(R.id.EtPassword);
+        show = (CheckBox) findViewById(R.id.show);
 
-        change.setOnClickListener(new myOnClickListenerImp1());
+        show.setOnClickListener(new OnClickListenerImp1());
     }
 
-    private class myOnClickListenerImp1 implements View.OnClickListener{
+    private class OnClickListenerImp1 implements View.OnClickListener{
         @Override
         public void onClick(View v){
-            if (MainActivity.this.getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED){
-                MainActivity.this.change.setText("错误！无法改变屏幕方向。");
-            }else if (MainActivity.this.getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE){
-                MainActivity.this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-            }else if (MainActivity.this.getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT){
-                MainActivity.this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            if (MainActivity.this.show.isChecked()){
+                MainActivity.this.password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            }else{
+                MainActivity.this.password.setTransformationMethod(PasswordTransformationMethod.getInstance());
             }
         }
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig){
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){
-            MainActivity.this.img.setImageResource(R.drawable.bg);
-            MainActivity.this.change.setText("改变当前横屏为竖屏显示");
-        }else if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
-            MainActivity.this.img.setImageResource(R.drawable.bg2);
-            MainActivity.this.change.setText("改变当前竖屏为横屏显示");
-        }
-        super.onConfigurationChanged(newConfig);
     }
 }
